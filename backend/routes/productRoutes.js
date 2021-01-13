f973c54b2525c289ce99c3e185cf56abd332b2ae
Maciej Kruszyniak <maciejkruszyniak@gmail.com>
2021-01-13 23:37:34 +0100

    back end user authentication

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,49 +1,34 @@
 import express from 'express'
 const router = express.Router()
-import asyncHandler from 'express-async-handler'
-import Products from '../models/productModel.js'
 
 
-// import {
-//     getProducts,
-//     getProductById,
+
+import {
+    getProducts,
+    getProductById,
 //     deleteProduct,
 //     createProduct,
 //     updateProduct,
 //     createProductReview,
 //     getTopProducts,
-// } from '../controllers/productController.js'
+} from '../controllers/productController.js'
 // import { protect, admin } from '../middleware/authMiddleware.js'
 
 // Fetch all products 
 // Route  GET /api/products
 // Access Public
-router.get('/', asyncHandler(async (req, res) => {
-    const products = await Products.find({})
-    res.json(products)
-}))
+router.route('/').get(getProducts)
 
 // Fetch single product
 // Route  GET /api/product/:id
 // Access Public
-router.get('/:id', asyncHandler(async(req, res) => {
-    const product =await Products.findById(req.params.id)
 
-    if(product){
-        res.json(product)
-    }else{
-        res.status(404)
-        throw new Error('Nie odnaleziono produktu koleżko.')
-    }
-
-}))
 
 // router.route('/').get(getProducts).post(protect, admin, createProduct)
 // router.route('/:id/reviews').post(protect, createProductReview)
 // router.get('/top', getTopProducts)
-// router
-//     .route('/:id')
-//     .get(getProductById)
+router.route('/:id')
+    .get(getProductById)
 //     .delete(protect, admin, deleteProduct)
 //     .put(protect, admin, updateProduct)
 
--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
-// import userRoutes from './routes/userRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 // import orderRoutes from './routes/orderRoutes.js'
 // import uploadRoutes from './routes/uploadRoutes.js'
 
@@ -26,10 +26,10 @@ const app = express()
 //     app.use(morgan('dev'))
 // }
 
-// app.use(express.json())
+app.use(express.json())
 
 app.use('/api/products', productRoutes)
-// app.use('/api/users', userRoutes)
+app.use('/api/users', userRoutes)
 // app.use('/api/orders', orderRoutes)
 // app.use('/api/upload', uploadRoutes)
 
