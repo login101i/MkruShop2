058ef66c7ca539f1b9010690bd51a91525ba44fc
Maciej Kruszyniak <maciejkruszyniak@gmail.com>
2021-01-14 23:34:05 +0100

    update

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,13 +6,13 @@ const router = express.Router()
 import {
     getProducts,
     getProductById,
-//     deleteProduct,
-//     createProduct,
-//     updateProduct,
+    deleteProduct,
+    createProduct,
+    updateProduct,
 //     createProductReview,
 //     getTopProducts,
 } from '../controllers/productController.js'
-// import { protect, admin } from '../middleware/authMiddleware.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
 
 // Fetch all products 
 // Route  GET /api/products
@@ -24,12 +24,14 @@ router.route('/').get(getProducts)
 // Access Public
 
 
-// router.route('/').get(getProducts).post(protect, admin, createProduct)
+router.route('/')
+.get(getProducts)
+.post(protect, admin, createProduct)
 // router.route('/:id/reviews').post(protect, createProductReview)
 // router.get('/top', getTopProducts)
 router.route('/:id')
     .get(getProductById)
-//     .delete(protect, admin, deleteProduct)
-//     .put(protect, admin, updateProduct)
+    .delete(protect, admin, deleteProduct)
+    .put(protect, admin, updateProduct)
 
 export default router
